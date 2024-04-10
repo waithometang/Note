18ae13d92b77b0ee1d126df985c4940836086751
whtang <weihongtang66@gmail.com>
2024-04-10 21:01:39 +0800

    4-2类型守卫补充in

--- Typescript/TSUpgrade/4chapter/src/4-2类型守卫.ts
+++ Typescript/TSUpgrade/4chapter/src/4-2类型守卫.ts
@@ -1,3 +1,5 @@
+// 类型守卫：会在块级作用域中缩小变量类型
+
 // 类型判断 typeof 检测范围：stirng,number,undefined,symbol,bigint,boolean,object,function
 const arr = [1, 2, 3];
 const map = new Map();
@@ -31,3 +33,14 @@ class Dog {
     }
   }
 }
+// typeof
+function getLen(num: number | string) {
+  if (typeof num === "number") {
+  } else {
+  }
+}
+// in如果指定的属性在指定的对象或其原型链中，则 in 运算符返回 true。
+let obj = { name: "zs", say: function () {}, age: 20 };
+console.log("name" in obj); // true
+
+export {};
