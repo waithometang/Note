241366389d98df6db30e0a5364a95de78f3a08a7
waithometang <164455576+waithometang@users.noreply.github.com>
2024-04-23 10:42:50 +0800

    feat：11-解构原理补充

--- JavaScript/抖音题目/codereasy/11-解构原理.js
+++ JavaScript/抖音题目/codereasy/11-解构原理.js
@@ -9,6 +9,38 @@ function* iterator() {
   yield 2;
 }
 Object.prototype[Symbol.iterator] = iterator;
+
+Object.prototype[Symbol.iterator] = function () {
+  return Object.values(this)[Symbol.iterator]();
+};
+const [a, b] = { a: 1, b: 2 }; // {(intermediate value)(intermediate value)} is not iterable
+
+function customGenerator() {
+  const data = [1, 2];
+  let index = 0;
+  return {
+    next() {
+      return index < data.length
+        ? {
+            value: data[index++],
+            done: false,
+          }
+        : {
+            value: undefined,
+            done: true,
+          };
+    },
+  };
+}
+
+let g = customGenerator();
+// { value: 1, done: false }
+// { value: 2, done: false }
+// { value: undefined, done: true }
+// console.log(g.next());
+// console.log(g.next());
+// console.log(g.next());
+
 const [a, b] = { a: 1, b: 2 };
 console.log(a, b); // 1 2
 
