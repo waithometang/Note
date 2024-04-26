7dd971943565000b49ad4dc12a45f2a21773f3de
waithometang <164455576+waithometang@users.noreply.github.com>
2024-04-26 15:28:14 +0800

    pref:：优化signalRManager

--- Vue/signalRManager.js
+++ Vue/signalRManager.js
@@ -1,77 +1,5 @@
-// import * as signalR from "@microsoft/signalr";
-// import { TOKEN } from "@/utils/constants";
-// import { getJWTToken } from "@/utils/otherMethod";
-// import { getItem } from "@/utils/storage";
-// import { getHost } from "@/utils/request.js";
-// import useStores from "@/store/modules";
-
-// class SignalRManager {
-//   constructor() {
-//     const { settingStore } = useStores();
-//     const token = getItem(TOKEN);
-//     let userInfo = {};
-//     if (token) {
-//       userInfo = getJWTToken(token);
-//     }
-//     const autoReconnect = [];
-//     for (let index = 0; index < 120; index++) {
-//       autoReconnect.push(5000);
-//     }
-//     this.connection = new signalR.HubConnectionBuilder()
-//       .withUrl(
-//         getHost() +
-//           `/StorageLocationHubService?language=${settingStore.language}&userName=${userInfo.UserName}&account=${userInfo.name}`,
-//         {}
-//       )
-//       .withAutomaticReconnect(autoReconnect)
-//       .configureLogging(signalR.LogLevel.Error)
-//       .build();
-
-//     this.eventListeners = {};
-//     this.startConnection();
-//   }
-//   startConnection() {
-//     this.connection
-//       .start()
-//       .then(() => {
-//         console.log("SignalR Connection Connected");
-//       })
-//       .catch((err) => {
-//         console.error("SignalR Connection Error: ", err);
-//       });
-//   }
-//   startListening(eventName, callback) {
-//     if (!this.eventListeners[eventName]) {
-//       this.eventListeners[eventName] = callback;
-//       this.connection.on(eventName, callback);
-//     }
-//   }
-//   stopListening(eventName) {
-//     if (this.eventListeners[eventName]) {
-//       this.connection.off(eventName, this.eventListeners[eventName]);
-//       delete this.eventListeners[eventName];
-//     }
-//   }
-//   stopConnection() {
-//     this.connection
-//       .stop()
-//       .then(() => {
-//         console.log("SignalR Connection Stopped");
-//       })
-//       .catch((err) => {
-//         console.error("Error stopping SignalR Connection: ", err);
-//       });
-//   }
-// }
-// const signalRManager = new SignalRManager();
-// export default signalRManager;
-
 import * as signalR from "@microsoft/signalr";
 import { TOKEN } from "@/utils/constants";
-import { getJWTToken } from "@/utils/otherMethod";
-import { getItem } from "@/utils/storage";
-import { getHost } from "@/utils/request.js";
-import useStores from "@/store/modules";
 
 class SignalRManager {
   constructor() {
@@ -81,7 +9,7 @@ class SignalRManager {
   }
 
   initConnection() {
-    const { settingStore } = useStores();
+    const { settingStore, rootStore } = useStores();
     const token = getItem(TOKEN);
     let userInfo = {};
     if (token) {
@@ -90,14 +18,14 @@ class SignalRManager {
     const autoReconnect = Array.from({ length: 120 }, () => 5000);
 
     this.connection = new signalR.HubConnectionBuilder()
-      .withUrl(
-        getHost() +
-          `/StorageLocationHubService?language=${settingStore.language}&userName=${userInfo.UserName}&account=${userInfo.name}`,
-        {}
-      )
+      .withUrl(url)
       .withAutomaticReconnect(autoReconnect)
       .configureLogging(signalR.LogLevel.Error)
       .build();
+    this.connection.onreconnected(() => {
+      console.log("SignalR Connection Reconnected");
+      rootStore.connectionId = this.connection.connectionId;
+    });
   }
 
   startConnection() {
