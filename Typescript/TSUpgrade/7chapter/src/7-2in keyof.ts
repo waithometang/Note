a4e1307c600741c22137979e4cbf1ecacfa4e8df
whtang <weihongtang66@gmail.com>
2024-05-01 10:00:32 +0800

    7-2in keyof

---
+++ Typescript/TSUpgrade/7chapter/src/7-2in keyof.ts
@@ -0,0 +1,27 @@
+type Gender = "male" | "female" | "未知";
+interface Person {
+  name: string;
+  age: number;
+  gender: Gender;
+}
+// in keyof相当于for in，遍历对象的key
+/**
+ * {
+    name: string;
+    age: number;
+    gender: Gender;
+    }
+ */
+type PersonType = {
+  [P in keyof Person]: Person[P];
+};
+// 定义泛型
+type GenericityType<T> = {
+  [K in keyof T]: T[K];
+};
+
+let p: GenericityType<Person> = {
+  name: "zs",
+  age: 10,
+  gender: "male",
+};
