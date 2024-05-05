8ceb26d2900b8b2042fb048b22a311ba1007450f
whtang <weihongtang66@gmail.com>
2024-05-05 21:47:26 +0800

    7-8Record深拷贝

--- Typescript/TSUpgrade/7chapter/src/7-8Record.ts
+++ Typescript/TSUpgrade/7chapter/src/7-8Record.ts
@@ -25,5 +25,42 @@ let rec: Record_ = {
   age: 20,
   [Symbol("foo")]: "foo",
 };
+type Test = Record<string, any>;
+let arr: Test = [1, 2, 3, 4, 5];
+let obj: Test = { name: "zs", age: 20 };
+
+// 对象深拷贝
+let copyObj = {
+  name: "lisi",
+  age: 123,
+  hobbies: ["ball", "play"],
+  info: {
+    address: "asndjasd",
+    phone: 123123,
+  },
+};
+type BaseType = string | number | boolean;
+type DeepCopyObject = Record<string, any>;
+function deepClone(
+  obj: DeepCopyObject | BaseType,
+  map = new WeakMap()
+): DeepCopyObject | BaseType {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+  if (map.has(obj)) {
+    return map.get(obj);
+  }
+  let result: DeepCopyObject = Array.isArray(obj) ? [] : {};
+  map.set(obj, result);
+  for (let key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      result[key] = deepClone((obj as DeepCopyObject)[key], map);
+    }
+  }
+  return result;
+}
+
+console.log(deepClone(copyObj));
 
 export {};
