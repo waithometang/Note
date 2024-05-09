0f46f58085705afaebba27ec58dfca4f319d9251
waithometang <164455576+waithometang@users.noreply.github.com>
2024-05-09 10:30:06 +0800

    7-2keyof补充

--- Typescript/TSUpgrade/7chapter/src/7-2in keyof.ts
+++ Typescript/TSUpgrade/7chapter/src/7-2in keyof.ts
@@ -1,3 +1,5 @@
+// The keyof operator takes an object type and produces a string or numeric literal union of its keys.
+// keyof 运算符采用对象类型并生成其键的字符串或数字文本并集。
 type Gender = "male" | "female" | "未知";
 interface Person {
   name: string;
@@ -25,4 +27,15 @@ let p: GenericityType<Person> = {
   age: 10,
   gender: "male",
 };
+
+type Point = {x:number;y:number;}
+type P = keyof Point // type P = "x" | "y"
+
+type ArrayIndex = { [n: number]: unknown }
+type AK = keyof ArrayIndex // type AK = number
+
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish // type M = string | number
+
+
 export {};
