1c3e577b3fbd1ae3158fd7e314996761276bf996
waithometang <weihongtang66@gmail.com>
2024-05-12 22:25:29 +0800

    7-13综合案例补充

--- Typescript/TSUpgrade/7chapter/src/7-13综合案例.ts
+++ Typescript/TSUpgrade/7chapter/src/7-13综合案例.ts
@@ -27,16 +27,35 @@ interface Todo {
 // type CapTyp = Capitalize<Cap>; // type CapTyp = "Hello"
 
 // 2、将获取到的属性加额外的操作，模板字符串
-type Degree<T> = {
-  [P in keyof T as T[P] extends Function
-    ? `do${Capitalize<P & string>}`
-    : never]: T[P];
-};
+// type Degree<T> = {
+//   [P in keyof T as T[P] extends Function
+//     ? `do${Capitalize<P & string>}`
+//     : never]: T[P];
+// };
 // type DegreeTodo = {
 //   doAdd: () => number;
 //   doDel: () => number;
 //   doUpdate: () => number;
 // }
-type DegreeTodo = Degree<Todo>;
+// type DegreeTodo = Degree<Todo>;
+
+// 对输入类型做限制，只能传入对象
+// 过滤掉数组
+type Exc<T> = Exclude<T, Array<any>>;
+type Degree<T extends Record<string, any>> = {
+  [P in keyof Exc<T> as Exc<T>[P] extends Function
+    ? `do${Capitalize<P & string>}`
+    : never]: Exc<T>[P];
+};
+// 需要对输入类型做限制
+// type DegreeLimit = Degree<1>;
+
+// 传数组也行，但是返回的数据类型是所有数组相关的方法
+type DegreeArray = Degree<Array<any>>; // type DegreeArray = {}
+type DegreeObj = Degree<Todo>;
+
+type Arr = keyof Array<any>;
+type Covert<T> = T extends any ? T : never;
+type Test = Covert<Arr>;
 
 export {};
