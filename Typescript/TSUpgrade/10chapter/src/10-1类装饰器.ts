eb600fb57775e879bf7d765e76d7bff2b9ba9814
waithometang <164455576+waithometang@users.noreply.github.com>
2024-06-03 20:33:42 +0800

    10-1类装饰器

--- Typescript/TSUpgrade/10chapter/src/10-1类装饰器.ts
+++ Typescript/TSUpgrade/10chapter/src/10-1类装饰器.ts
@@ -19,3 +19,27 @@ class Product {
     console.log(this.name + "价格是：9000");
   }
 }
+// 带参数类装饰器
+function ProductDecorator1(arg: string) {
+    console.log(arg);
+    return function (targetClass: any) {
+        // targetClass实际上就是类
+        const target = new targetClass("iphone");
+        target.buy(); // 购买iphone
+    }
+}
+@ProductDecorator1("小明")
+class Product1 {
+    name: string = "iphone";
+    constructor(name: string) {
+        this.name = name;
+    }
+    buy() {
+        console.log("购买" + this.name);
+    }
+
+    price() {
+        console.log(this.name + "价格是：9000");
+    }
+}
+export {}
