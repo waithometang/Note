b7ecbc7c5cfe9a716c74a34cfbb3c3dc25795117
waithometang <164455576+waithometang@users.noreply.github.com>
2024-07-11 20:23:22 +0800

    属性注入实现，安装reflect-metadata

--- Typescript/TSUpgrade/10chapter/src/10-5属性、类、方法装饰器综合使用/Injectdecorator.ts
+++ Typescript/TSUpgrade/10chapter/src/10-5属性、类、方法装饰器综合使用/Injectdecorator.ts
@@ -1,1 +1,19 @@
-export {};
+type MyPropDecorator = (
+  targetClassProperty: any,
+  propertyKey: string | symbol
+) => void;
+
+import "reflect-metadata"; // 获取元数据
+// injectid:参数
+export function Inject(injectid: string): MyPropDecorator {
+  // targetClassProperty:类原型对象，propertyKey:属性名称
+  return (targetClassProperty, propertyKey) => {
+    // UserService
+    let PropClass = Reflect.getMetadata(
+      "design:type",
+      targetClassProperty,
+      propertyKey
+    );
+    let PropClassObj = new PropClass();
+  };
+}
