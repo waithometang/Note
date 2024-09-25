3be60596f69c10b95d87dea13c891c6f6e65b3de
waithometang <weihongtang66@gmail.com>
2024-09-25 22:46:14 +0800

    feat：使用vite+vue3初始化cesium项目

---
+++ 3D/Cesium/vue-cesium-demo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+console.log(Cesium);
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYTZkZTU3MS0wYTI4LTQ0NDQtYmY4My05ZjA4Yjk2YmMwZGYiLCJpZCI6MjQ0MDY4LCJpYXQiOjE3MjcyNzMxOTd9.oXrtk8x_ASVxQQD8tIA2-xTGxJqUIbmhL7jNekq-_m4";
+  new Cesium.Viewer("cesiumContainer");
+});
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
