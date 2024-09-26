6b6cf8ea06d0dc1cab90c290d3bec35c08a69f4a
waithometang <weihongtang66@gmail.com>
2024-09-26 23:18:36 +0800

    feat：cesium线

---
+++ 3D/Cesium/vue-cesium-demo/src/components/07-线.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+console.log(Cesium);
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYTZkZTU3MS0wYTI4LTQ0NDQtYmY4My05ZjA4Yjk2YmMwZGYiLCJpZCI6MjQ0MDY4LCJpYXQiOjE3MjcyNzMxOTd9.oXrtk8x_ASVxQQD8tIA2-xTGxJqUIbmhL7jNekq-_m4";
+
+  const viewer = new Cesium.Viewer("cesiumContainer", {});
+  // 线 描述折线。前两个位置定义线段，并且每个其他位置都从前一个位置定义了一个线段
+  const polyline = viewer.entities.add({
+    polyline: {
+      positions: Cesium.Cartesian3.fromDegreesArray([122, 32, 125, 35, 45, 56]),
+      width: 10,
+      material: Cesium.Color.CORAL,
+    },
+  });
+  viewer.zoomTo(polyline);
+});
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
