8cb27bcf903f90ada8a7f9ac8f11cb050821f42c
waithometang <weihongtang66@gmail.com>
2024-09-26 23:42:11 +0800

    feat：cesium多边形

---
+++ 3D/Cesium/vue-cesium-demo/src/components/08-多边形.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+console.log(Cesium);
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYTZkZTU3MS0wYTI4LTQ0NDQtYmY4My05ZjA4Yjk2YmMwZGYiLCJpZCI6MjQ0MDY4LCJpYXQiOjE3MjcyNzMxOTd9.oXrtk8x_ASVxQQD8tIA2-xTGxJqUIbmhL7jNekq-_m4";
+
+  const viewer = new Cesium.Viewer("cesiumContainer", {});
+
+  // 描述由构成外部形状和任何嵌套孔的线性环的层次结构定义的多边形
+  const polygon = viewer.entities.add({
+    polygon: {
+      hierarchy: {
+        positions: Cesium.Cartesian3.fromDegreesArray([
+          120, 25, 121, 25, 121, 25.5,
+        ]),
+      },
+      height: 100000,
+      extrudedHeight: 20000,
+      material: Cesium.Color.RED,
+      outline: true,
+      outlineColor: Cesium.Color.BLUE,
+      fill: false,
+    },
+  });
+  viewer.zoomTo(polygon);
+});
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
