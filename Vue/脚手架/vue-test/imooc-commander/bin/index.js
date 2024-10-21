a5e54ce580103b36f779069355c70c5506f0aa8b
waithometang <weihongtang66@gmail.com>
2024-10-21 21:50:27 +0800

    feat：commander脚手架全局配置

--- Vue/脚手架/vue-test/imooc-commander/bin/index.js
+++ Vue/脚手架/vue-test/imooc-commander/bin/index.js
@@ -5,7 +5,23 @@
 const commander = require("commander");
 
 const pkg = require("../package.json");
-
+// 获取commander实例
 const { program } = commander;
 
-program.version(pkg.version).parse(process.argv);
+// 手动实例一个commander
+// const program = new commander.Command();
+
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage("<command> [options]")
+  .version(pkg.version)
+  .option("-d --debug", "output extra debugging", true)
+  .option("-e --envName <envName>", "get env name")
+  .parse(process.argv);
+
+// 获取参数选项
+const options = program.opts();
+// 输出帮助信息,相当于-h
+program.outputHelp();
+// { debug: true, envName: 'xxx' }
+console.log(options);
