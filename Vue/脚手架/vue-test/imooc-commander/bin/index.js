186e68f2a19a0de7db190a16f45ff6e0079d6701
waithometang <weihongtang66@gmail.com>
2024-10-21 22:45:21 +0800

    feat：脚手架命令注册两种方法

--- Vue/脚手架/vue-test/imooc-commander/bin/index.js
+++ Vue/脚手架/vue-test/imooc-commander/bin/index.js
@@ -16,12 +16,43 @@ program
   .usage("<command> [options]")
   .version(pkg.version)
   .option("-d --debug", "output extra debugging", true)
-  .option("-e --envName <envName>", "get env name")
-  .parse(process.argv);
+  .option("-e --envName <envName>", "get env name");
 
 // 获取参数选项
-const options = program.opts();
+// const options = program.opts();
 // 输出帮助信息,相当于-h
-program.outputHelp();
 // { debug: true, envName: 'xxx' }
-console.log(options);
+// console.log(options);
+
+// 注册命令：command
+// 参数可为必选的（尖括号表示）、可选的（方括号表示）或变长参数（点号表示，如果使用，只能是最后一个参数）
+const clone = program.command("clone <source> [destination]");
+clone
+  .description("clone a repository into a newly created directory")
+  .option("-f --force", "是否强制克隆")
+  .action((source, destination, cmdObj) => {
+    console.log("clone command called", source, destination, cmdObj.force);
+  });
+
+// 注册子命令：addCommand
+// imooc-yargs service start/stop
+const service = new commander.Command("service");
+service
+  .command("start [port]")
+  .description("start service")
+  .action((port) => {
+    console.log("start service at", port);
+  });
+
+service
+  .command("stop")
+  .description("stop service")
+  .action(() => {
+    console.log("stop service");
+  });
+
+program.addCommand(service);
+
+// 这个需要放在最后，用来解析参数
+program.parse(process.argv);
+// program.outputHelp();
