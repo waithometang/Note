59ede691d1523480fa2954782e06df8eb36c8b57
waithometang <weihongtang66@gmail.com>
2024-10-25 22:07:14 +0800

    feat：inquirer各种交互方式演示

--- Vue/脚手架/vue-test/inquirer/inquirer/index.js
+++ Vue/脚手架/vue-test/inquirer/inquirer/index.js
@@ -4,21 +4,89 @@ const inquirer = require("inquirer");
 
 const prompt = inquirer.createPromptModule();
 
-prompt({
-  type: "input",
-  name: "youName",
-  message: "input your name",
-  default: "twh",
-  validate: function (v) {
-    return typeof v === "string";
+prompt([
+  // {
+  //   type: "input",
+  //   name: "youName",
+  //   message: "input your name",
+  //   default: "twh",
+  //   validate: function (v) {
+  //     return typeof v === "string";
+  //   },
+  //   filter: function (f) {
+  //     return "Name: " + f;
+  //   },
+  //   transformer: function (t) {
+  //     return t + "(input your name?)";
+  //   },
+  // },
+  // {
+  //   type: "number",
+  //   name: "number",
+  //   message: "input number",
+  // },
+  // {
+  //   type: "confirm",
+  //   name: "select",
+  //   message: "yes/no",
+  //   default: false,
+  // },
+  // {
+  //   type: "list",
+  //   name: "choice",
+  //   message: "choices",
+  //   default: 2,
+  //   choices: [
+  //     { name: "1", value: 1 },
+  //     { name: "2", value: 2 },
+  //     { name: "3", value: 3 },
+  //   ],
+  // },
+  // {
+  //   type: "rawlist",
+  //   name: "choice",
+  //   message: "choices",
+  //   default: 2,
+  //   choices: [
+  //     { name: "1", value: 1 },
+  //     { name: "2", value: 2 },
+  //     { name: "3", value: 3 },
+  //   ],
+  // },
+  // {
+  //   type: "expand",
+  //   name: "choice",
+  //   message: "choices",
+  //   default: "r",
+  //   choices: [
+  //     { key: "r", value: 10 },
+  //     { key: "g", value: 20 },
+  //     { key: "b", value: 30 },
+  //   ],
+  // },
+  // {
+  //   type: "checkbox",
+  //   name: "choice",
+  //   message: "choices",
+  //   // default: [20, 30],
+  //   choices: [
+  //     { key: "r", value: 10, disabled: true },
+  //     { key: "g", value: 20, checked: true },
+  //     { key: "b", value: 30 },
+  //   ],
+  // },
+  // {
+  //   type: "password",
+  //   name: "password",
+  //   message: "input password",
+  //   mask: "*",
+  // },
+  {
+    type: "editor",
+    name: "editor",
+    message: "input content:",
   },
-  filter: function (f) {
-    return "Name: " + f;
-  },
-  transformer: function (t) {
-    return t + "(input your name?)";
-  },
-})
+])
   .then((answer) => {
     console.log(answer);
   })
